cb737b2d6b7a1ed62b4a2c09a2c41d4dda1c53e3
Annimi666 <2371225863@qq.com>
2021-11-20 16:32:38 +0800

    Use of the Stanford NLP package

---
+++ src/test/java/zut/zn/stanford/nlp_zn/NlpZnApplicationTests.java
@@ -0,0 +1,13 @@
+package zut.zn.stanford.nlp_zn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlpZnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
